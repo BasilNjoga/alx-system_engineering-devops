ba36c48c235e5be45297901cab5ea96d6b6c70c3
basil <basilnjoga@gmail.com>
2022-09-21 10:53:41 -0700

    check

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -3,10 +3,10 @@
 node default {
 
 file { '/tmp/school':
-  ensure => present
+  ensure             => present
   source_permissions => '0744'
-  owner => 'www-data'
-  group => 'www-data'
-  content => 'I love Puppet'
+  owner              => 'www-data'
+  group              => 'www-data'
+  content            => 'I love Puppet'
   }
 }
