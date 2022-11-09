d61e12818f87494d4f77d9935ce9f4aef00167bf
Basil Njoga <Basilnjoga@gmail.com>
2022-11-09 16:25:23 +0300

    check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" This script fetches a RESTapi and gets a response """
+
+import requests
 
 fetch('https://jsonplaceholder.typicode.com/todos/1')
   .then(response => response.json())
