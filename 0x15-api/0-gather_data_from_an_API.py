47ccfcb28d178482f00981f352b0841c29a09fea
basil <basilnjoga@gmail.com>
2022-11-09 14:09:46 +0000

    fixed errors

--- 0x15-api/0-gather_data_from_an_API.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" This script fetches a RESTapi and gets a response """
-
-import requests
-
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => console.log(json))
