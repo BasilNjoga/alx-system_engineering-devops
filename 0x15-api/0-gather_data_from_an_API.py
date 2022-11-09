530db1e3eb8b9b4d06832576441fe2c455c7ef48
basil <basilnjoga@gmail.com>
2022-11-09 14:16:49 +0000

    chech

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,2 +1,5 @@
 #!/usr/bin/python3
 """ This is a file on calling data from a rest api """
+import requests
+response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
+print(response.json)
