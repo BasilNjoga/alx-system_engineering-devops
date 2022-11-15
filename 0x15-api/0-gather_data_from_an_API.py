4ab46a355e4032e956b0edfccc9d9b2b566deca0
basil <basilnjoga@gmail.com>
2022-11-15 08:10:05 -0800

    fix: fixed json functionality

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
 """ This is a file on calling data from a rest api """
 import requests
-response = requests.get('https://jsonplaceholder.typicode.com/todos/1')
-print(response.text)
+from sys import argv
+
+users = "https://jsonplaceholder.typicode.com/users"
+todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+users = requests.get(f"https://jsonplaceholder.typicode.com/users/1")
+
+data = users.json()
+print(data)
+print(users)
+print(data["name"])
+#print("Employee {} is done with".format(data['name']))
