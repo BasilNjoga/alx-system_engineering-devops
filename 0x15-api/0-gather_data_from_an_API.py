564d95a28ba7a8423f588120cec9d1430060f3cf
basil <basilnjoga@gmail.com>
2022-11-15 10:15:23 -0800

    check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -3,22 +3,24 @@
 import requests
 from sys import argv
 
-theid = argv[1]
-url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
-todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-users = requests.get(url)
-tasks = 0
-complete = 0
+if __name__ == "__main__":
+    """ This calls from a rest api"""
+    theid = int(argv[1])
+    url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get(url)
+    tasks = 0
+    complete = 0
 
-data = users.json()
-numbers = todos.json()
-for obj in numbers:
-    if obj['userId'] == 1:
-        tasks = tasks + 1
-        if obj['completed'] == True:
-            complete = complete + 1
-print(theid)
-print("Employee {} is done with tasks ({}/{}):".format(data['name'], complete, tasks))
-for obj in numbers:
-    if ((obj['userId'] == 1) & (obj['completed'] == True)):
+    data = users.json()
+    numbers = todos.json()
+    for obj in numbers:
+        if obj['userId'] == theid:
+            tasks = tasks + 1
+            if obj['completed'] == True:
+                complete = complete + 1
+    print(theid)
+    print("Employee {} is done with tasks ({}/{}):".format(data['name'], complete, tasks))
+    for obj in numbers:
+        if ((obj['userId'] == theid) & (obj['completed'] == True)):
             print("\t {}".format(obj['title']))
