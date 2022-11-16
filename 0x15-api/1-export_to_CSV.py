70a05a1f2fc661d3ca574b3ec60569c7ceaf409b
basil <basilnjoga@gmail.com>
2022-11-16 01:42:45 -0800

    check

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,2 +1,27 @@
 #!/usr/bin/python3
-""" This module experts to csv """
+""" This scirpt exports data in csv format"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    """ runs only when function is called """
+    theid = int(argv[1])
+    url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get(url)
+    tasks = 0
+    complete = 0
+    data = users.json()
+    numbers = todos.json()
+    for obj in numbers:
+        if obj['userId'] == theid:
+            tasks = tasks + 1
+            if obj['completed'] is True:
+                complete = complete + 1
+    """ prints out data """
+    out = ""
+    out = ("Employee {} is done with tasks".format(data['name']))
+    print(out + "({}/{}):".format(complete, tasks))
+    for obj in numbers:
+        if ((obj['userId'] == theid) & (obj['completed'] is True)):
+            print("\t {}".format(obj['title']))
