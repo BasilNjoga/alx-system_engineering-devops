8c986c8e4ab22baa7b408866030d94e3d06d07f1
basil <basilnjoga@gmail.com>
2022-11-16 00:28:07 -0800

    check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-""" This is a file on calling data from a rest api """
+"""This is a file on calling data from a rest api """
 import requests
 from sys import argv
 
 if __name__ == "__main__":
     """This calls from a rest api and displays ouput"""
-
-    theid = argv[1]
+    theid = int(argv[1])
     url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
     todos = requests.get('https://jsonplaceholder.typicode.com/todos')
     users = requests.get(url)
@@ -22,7 +21,6 @@ if __name__ == "__main__":
                 complete = complete + 1
     out = f"Employee {data['name']} is done with tasks "
     print(out + "({}/{}):".format(complete, tasks))
-
     """ This loops through to get the tasks completed """
     for obj in numbers:
         if ((obj['userId'] == theid) & (obj['completed'] is True)):
