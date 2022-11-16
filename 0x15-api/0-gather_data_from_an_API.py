f8ccb38e900ced0e0c8d241dfa73e4b5804a9fcc
basil <basilnjoga@gmail.com>
2022-11-16 00:21:17 -0800

    check

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     users = requests.get(url)
     tasks = 0
     complete = 0
-
     data = users.json()
     numbers = todos.json()
+    """ This loops trhough to print name """
     for obj in numbers:
         if obj['userId'] == theid:
             tasks = tasks + 1
@@ -22,6 +22,8 @@ if __name__ == "__main__":
                 complete = complete + 1
     out = f"Employee {data['name']} is done with tasks "
     print(out + "({}/{}):".format(complete, tasks))
+
+    """ This loops through to get the tasks completed """
     for obj in numbers:
         if ((obj['userId'] == theid) & (obj['completed'] is True)):
             print("\t {}".format(obj['title']))
