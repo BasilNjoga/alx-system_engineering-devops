f906ef6989969d7a7c2018374a7f291640dd419c
basil <basilnjoga@gmail.com>
2022-11-17 05:48:26 -0800

    check

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 """ returns number of subscribers on an subreddit """
+import requests
+
+def number_of_subscribers(subreddit):
+    url = ("GET [/r/{}]/about/where".format(subbreddit))
+    r = requests.get("GET [/r/subreddit]/about/contributors")
+
+    return(r.json())
