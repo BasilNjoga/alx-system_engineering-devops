61da7556daed2f366daed4dde9857b9df609fe22
basil <basilnjoga@gmail.com>
2022-11-17 01:43:31 -0800

    check

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,14 +8,15 @@ if __name__ == "__main__":
     """ This exports a list in json format """
 
     url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
-    todos = request.get("https://jsonplaceholder.typicode.com/todos/")
-    usersdata = request.get(url)
+    todos = requests.get("https://jsonplaceholder.typicode.com/todos/")
+    usersdata = requests.get(url)
 
     username = usersdata.json().get('username')
-    filename = argv[0] + ".json"
-
+    filename = argv[1] + ".json"
+    keys = ('task', 'completed', 'username')
     for obj in todos.json():
-        if obj.get('userId') == int(argv[0]):
-            thisdict = dict.fromkeys('task',obj.get('title'))
+        if obj.get('userId') == int(argv[1]):
+            values = (obj.get('title'), obj.get('completed'), username)
+            thisdict = dict.fromkeys(keys, values)
 
     print(thisdict)
