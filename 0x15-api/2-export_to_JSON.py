b883fb44c8b3ccfcb2c15ce21aebc0af46ee916a
Basil Njoga <Basilnjoga@gmail.com>
2022-11-17 11:48:18 +0300

    check

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -3,3 +3,19 @@
 
 import requests
 from sys import argv
+
+if __name__ == "__main__":
+    """ This exports a list in json format """
+
+    url = "https://jsonplaceholder.typicode.com/users/" + argv[1]
+    todos = request.get("https://jsonplaceholder.typicode.com/todos/")
+    usersdata = request.get(url)
+
+    username = usersdata.json().get('username')
+    filename = argv[0] + ".json"
+
+    for obj in todos.json():
+        if obj.get('userId') == int(argv[0]):
+            thisdict = dict.fromkeys('task',obj.get('title'))
+
+    print(thisdict)
